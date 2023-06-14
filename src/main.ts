ea790a114418bf42e68b697916339ff350d12f88
Willian17 <willianfernandes113@gmail.com>
2023-06-14 13:30:44 -0400

    refactor: interceptor error message default

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: 'http://localhost:4200' });
   await app.listen(3000);
 }
 bootstrap();
