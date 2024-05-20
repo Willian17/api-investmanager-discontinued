223ee86aea6779ebd3f7e2300e798724d86d8776
Willian17 <willianfernandes113@gmail.com>
2024-05-20 19:59:46 -0400

    refactor: liest controller marks

--- src/application/modules/marks.module.ts
+++ src/application/modules/marks.module.ts
@@ -3,10 +3,24 @@ import { MarksService } from './marks/marks.service';
 import { MarksController } from '../../adapters/input/controllers/marks/marks.controller';
 import { MarkEntity } from '../../adapters/output/marks/marks.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ListMarksUseCasePort } from '../port/input/list-marks.usecase.port';
+import { ListMarksUseCase } from '../usecase/marks/list-marks.usecase';
+import { MarksAccessDataAdapter } from 'src/adapters/output/marks/marks-access-data.adapter';
+import { MarksAccessDataPort } from '../port/output/marks-access-data.port';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MarkEntity])],
-  providers: [MarksService],
+  providers: [
+    MarksService,
+    {
+      provide: ListMarksUseCasePort,
+      useClass: ListMarksUseCase,
+    },
+    {
+      provide: MarksAccessDataPort,
+      useClass: MarksAccessDataAdapter,
+    },
+  ],
   controllers: [MarksController],
   exports: [MarksService],
 })
--- test/marks.e2e-spec.ts
+++ test/marks.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('MarksController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('(GET) /marks ', () => {
+    return request(app.getHttpServer()).get('/marks').expect(200);
   });
 });
